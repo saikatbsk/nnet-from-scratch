6f40873d88e0a8e95c785c72fb42bb9f7835cfd8
saikatbsk <saikatbsk@gmail.com>
2018-07-30 01:15:41 +0530

    predict function returns only Ypred

--- classifiers/nearestNeighbor.py
+++ classifiers/nearestNeighbor.py
@@ -4,7 +4,7 @@ class nearestNeighbor:
     def __init__(self):
         pass
 
-    def train(self, X, y):
+    def fit(self, X, y):
         # X is (N x D) where each row is an example.
         # y is 1 dimensional of size N.
         self.Xtr = X
@@ -13,7 +13,6 @@ class nearestNeighbor:
     def predict(self, X, metric='l2'):
         numTest = X.shape[0]
         Ypred = np.zeros(numTest, dtype=self.ytr.dtype)
-        Ydist = np.zeros(numTest, dtype=np.float32)
 
         for i in np.arange(numTest):
             if metric == 'l2':
@@ -25,6 +24,5 @@ class nearestNeighbor:
 
             minIndex = np.argmin(distances)
             Ypred[i] = self.ytr[minIndex]
-            Ydist[i] = distances[minIndex] / np.max(distances)
 
-        return Ypred, Ydist
+        return Ypred
